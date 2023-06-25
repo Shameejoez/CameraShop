be4e1f39a452535931d98ee0903f3eb018ed4a6c
Shamee <nachozzzz224@gmail.com>
2023-06-25 16:16:02 +0300

    mini fix

--- project/src/components/Review/Review-Item/Review-item.test.tsx
+++ project/src/components/Review/Review-Item/Review-item.test.tsx
@@ -1,5 +1,5 @@
 import { screen, render } from '@testing-library/react';
-import ReviewItem from './ReviewItem';
+import ReviewItem from './Review-item';
 import { Review } from '../../../types/types';
 
 
