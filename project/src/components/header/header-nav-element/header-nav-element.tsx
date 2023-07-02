96ffdf6dd8b32562432e6c8f6b7e504067c32e96
Shamee <nachozzzz224@gmail.com>
2023-07-02 14:17:11 +0300

    Header links fix

--- project/src/components/header/header-nav-element/header-nav-element.tsx
+++ project/src/components/header/header-nav-element/header-nav-element.tsx
@@ -6,7 +6,7 @@ type HeaderNavElementProps = {
 
 function HeaderNavElement ({name}: HeaderNavElementProps): JSX.Element {
   return (
-    <li className="main-nav__item"><a className="main-nav__link" href="catalog.html">{name}</a>
+    <li className="main-nav__item"><a className="main-nav__link" href="#">{name}</a>
     </li>
   );
 }
